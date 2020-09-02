20652c5663b6d2cfe1756307ce3cbe42963fc32a
Harish Karthic <harish.karthic@gmail.com>
2020-09-02 14:15:15 +0100

    Added psake build file

---
+++ build.ps1
@@ -0,0 +1,67 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false, Position = 0)]
+    [string] $ModuleName = (Get-ProjectName),
+
+    [Parameter(Mandatory = $false, Position = 1)]
+    [ValidateSet("Major", "Minor", "Patch", "Build")]
+    [string] $Version = "Patch"
+)
+
+# declaring constants
+$root = Split-Path $PSCommandPath
+$contents = @()
+$folders = @("Classes", "Private", "Public")
+
+
+Task init {
+    # module refresh
+    if (Get-Module -Name $ModuleName) {
+        Remove-Module -Name $ModuleName -Force
+    }
+}
+
+Task build {  
+
+    # move all the functions to module file and export only public functions.
+    foreach ($folder in $folders) {
+        if ($folder -eq "Classes") {
+            $class = Get-Content (Get-ChildItem -Path "$root\$folder" -Filter "*.ps1").FullName
+            $class | Set-Content "$root\$ModuleName\$ModuleName.classes.ps1"
+        } else {
+            $contents += Get-Content (Get-ChildItem -Path "$root\$folder" -Filter "*.ps1").FullName
+        }
+    }
+
+    $contents | Set-Content "$root\$ModuleName\$ModuleName.functions.ps1"
+}
+
+Task updateManifest {
+    # import and copy only public functions to manifest file.
+    Import-Module "$root\$ModuleName\$ModuleName.psm1" -Force
+
+    # $functions = Get-ModuleFunction
+
+    Step-ModuleVersion -Path (Get-PSModuleManifest) -By $Version
+}
+
+Task import {
+    # forcing the import of final module
+    Import-Module "$root\$ModuleName" -Force
+}
+
+Task createMarkdownHelp {
+    # generate markdown files.
+    New-MarkdownHelp -Module $ModuleName -OutputFolder "$root\docs" -ErrorAction SilentlyContinue
+}
+
+Task updateMarkdownHelp {
+    Update-MarkdownHelp -Path "$root\docs"
+}
+
+Task createExternalHelp {
+    # generate help files.
+    New-ExternalHelp -Path "$root\docs" -OutputPath "$root\$ModuleName\en-US" -Force
+}
+
+Task default -depends init,build,import
---
+++ psake.ps1
@@ -0,0 +1,1 @@
+Invoke-psake .\build.ps1
