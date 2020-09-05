5e7038fd920b832e3d048fa4af7b6739ed3766fa
Harish <harish.karthic@gmail.com>
2020-09-05 13:38:20 +0100

    Suppress message for SupportShouldProcess

--- PSDB/PSDB.functions.ps1
+++ PSDB/PSDB.functions.ps1
@@ -478,9 +478,8 @@ function Invoke-PSDBDatabaseQuery {
     }
 }
 function New-PSDBConnectionString {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', Justification = "Ths functions creates a connection
-    string and doesn't perform any state changing action.")]
-    [CmdletBinding(DefaultParameterSetName = "AAD")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
+    [CmdletBinding(DefaultParameterSetName = "AADIntegrated")]
     param (
         [Parameter(Mandatory = $true, ParameterSetName = "Standard")]
         [Parameter(Mandatory = $true, ParameterSetName = "MARSEnabled")]
