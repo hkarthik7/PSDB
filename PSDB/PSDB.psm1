02a2eb1bfb24ad6184ea0d623f3a90f81ac77f4d
Harish <harish.karthic@gmail.com>
2020-09-07 14:34:39 +0100

    Updated with kevault major update supress message

--- PSDB/PSDB.psm1
+++ PSDB/PSDB.psm1
@@ -1,3 +1,6 @@
 # dot sourcing all the files.
 . "$PSScriptRoot\PSDB.classes.ps1"
 . "$PSScriptRoot\PSDB.functions.ps1"
+
+#Supress the warning message for Key vault
+Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
