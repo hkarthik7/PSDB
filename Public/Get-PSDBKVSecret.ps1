ba32a6a854e395239fe4c521afc8fe2f7c967cc2
Harish <harish.karthic@gmail.com>
2020-09-07 14:35:49 +0100

    Major code refactor and bug fixes. Added error handling

--- Public/Get-PSDBKVSecret.ps1
+++ Public/Get-PSDBKVSecret.ps1
@@ -4,6 +4,7 @@ function Get-PSDBKVSecret {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [KeyVaultValidateAttribute()]
         [ArgumentCompleter([KeyVaultCompleter])]
         [ValidateNotNullOrEmpty()]
         [string] $VaultName,
@@ -21,48 +22,68 @@ function Get-PSDBKVSecret {
     process {
         try {
 
-            if ($PSBoundParameters["Version"]) {
+            $kvSecret = Get-AzKeyVaultSecret -VaultName $VaultName -Name $SecretName -IncludeVersions -ErrorAction stop
 
-                $kvSecret = Get-AzKeyVaultSecret -VaultName $VaultName -Name $SecretName -Version $Version
+            if ($null -eq $kvSecret) {
+                $Message = "Cannot validate argument on parameter 'SecretName'. '$($SecretName)' is not a valid SecretName. Pass the valid secret name and try again."
+                $ErrorId = "InvalidArgument,PSDBKVSecret\Get-PSDBKVSecret" 
+                Write-Error -Exception ArgumentException -Message $Message -Category InvalidArgument -ErrorId $ErrorId
+            }
 
-                if ($AsPlainText.IsPresent) {
-                    if ($kvSecret.Enabled) {
-                        return $kvSecret.SecretValueText
-                    } else {
-                        Write-Error "Given secret $($SecretName) is not enabled.."
-                    }
-                } else {
-                    return $kvSecret.SecretValue
-                }
-
-            } 
-            
             else {
-                $kvSecrets = Get-AzKeyVaultSecret -VaultName $VaultName -Name $SecretName -IncludeVersions
-                $secrets = @()
+                if ($PSBoundParameters["Version"]) {
+                    $secret = $kvSecret | Where-Object { $_.Version -eq $Version }
 
-                if ($AsPlainText.IsPresent) {
-                    $kvSecrets | ForEach-Object {
-                        if ($PSItem.Enabled) {
-                            $secrets += Get-AzKeyVaultSecret -VaultName $VaultName -Name $SecretName -Version $PSItem.Version
-                        }
+                    if ($null -eq $secret) {
+                        $Message = "Cannot validate argument on parameter 'Version'. '$($Version)' is not a valid Version number. Pass the valid version number and try again."
+                        $ErrorId = "InvalidArgument,PSDBKVSecret\Get-PSDBKVSecret" 
+                        Write-Error -Exception ArgumentException -Message $Message -Category InvalidArgument -ErrorId $ErrorId
                     }
 
-                    return $secrets.SecretValueText
-
-                } else {
-                    $kvSecrets | ForEach-Object {
-                        if ($PSItem.Enabled) {
-                            $secrets += Get-AzKeyVaultSecret -VaultName $VaultName -Name $SecretName -Version $PSItem.Version
+                    else {
+                        if ($AsPlainText.IsPresent) {
+                            if ($secret.Enabled) {
+                                $kv = Get-AzKeyVaultSecret -VaultName $secret.VaultName -Name $secret.Name -Version $secret.Version
+                                $PSCmdlet.WriteObject((_convertToPlainText ($kv.SecretValue -as [securestring])))
+                            } else {
+                                $Message = "Cannot validate argument on parameter 'SecretName'. '$($SecretName)' is not enabled. Pass the valid secret name and try again."
+                                $ErrorId = "InvalidArgument,PSDBKVSecret\Get-PSDBKVSecret" 
+                                Write-Error -Exception ArgumentException -Message $Message -Category InvalidArgument -ErrorId $ErrorId
+                            }
+                        } else {
+                            $kv = Get-AzKeyVaultSecret -VaultName $secret.VaultName -Name $secret.Name -Version $secret.Version
+                            $PSCmdlet.WriteObject($kv.SecretValue)
                         }
                     }
-
-                    return $secrets.SecretValue
                 }
+
+                else {
+                    if ($AsPlainText.IsPresent) {
+                        $kvSecret | ForEach-Object {
+                            if ($_.Enabled) {
+                                $kv = Get-AzKeyVaultSecret -VaultName $_.VaultName -Name $_.Name -Version $_.Version
+                                $PSCmdlet.WriteObject((_convertToPlainText ($kv.SecretValue -as [securestring])))
+                            }
+                        }
+                    } else {
+                        $kvSecret | ForEach-Object {
+                            if ($_.Enabled) {
+                                $kv = Get-AzKeyVaultSecret -VaultName $_.VaultName -Name $_.Name -Version $_.Version
+                                $PSCmdlet.WriteObject($kv.SecretValue)
+                            }
+                        }
+                    }
+                }                
             }
         }
         catch {
-            throw "Error at line $($_.InvocationInfo.ScriptLineNumber) : $($_.Exception.Message)."
+            $content = $_.Exception.Response.Content
+            $content = if ($content) { $content | ConvertFrom-Json }
+            if ($content.error.innererror.code -eq "SecretDisabled") {
+                $Message = "Cannot validate argument on parameter 'SecretName'. There is no active current version of '$($SecretName)'. Pass the valid secret name and try again."
+                $ErrorId = "InvalidArgument,PSDBKVSecret\Get-PSDBKVSecret" 
+                Write-Error -Exception ArgumentException -Message $Message -Category InvalidArgument -ErrorId $ErrorId
+            }
         }
     }
 }
