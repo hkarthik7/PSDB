922e61f64ff49c6bb222010675e100a3291b6f86
Harish <harish.karthic@gmail.com>
2020-09-07 18:10:30 +0100

    Test initial commit

---
+++ Tests/Get-PSDBConnectionString.Tests.ps1
@@ -0,0 +1,28 @@
+Set-StrictMode -Version Latest
+
+Describe "PSDB" {
+    BeforeAll {
+        Import-Module .\PSDB\PSDB.psm1
+    }
+    Context "Get-PSDBConnectionString" {
+        BeforeAll {
+            $result = @{
+                "Standard" = "Server=tcp:myserver.database.windows.net,1433;Database=myDataBase;User ID=mylogin@myserver;Password=myPassword;Trusted_Connection=False;Encrypt=True;"
+                "MARS Enabled"  = "Server=tcp:myserver.database.windows.net,1433;Database=myDataBase;User ID=mylogin@myserver;Password=myPassword;Trusted_Connection=False;Encrypt=True;MultipleActiveResultSets=True;"
+            }
+        }
+
+        It "Should be exactly same as result" {
+            Mock Get-PSDBConnectionString { retun $result }
+        }
+
+        It "Should return Az Sql Connection strings" {
+            Get-PSDBConnectionString | Should -BeOfType Hashtable
+        }
+
+        It "Should return corresponding value" {
+            $cs = "Server=tcp:myserver.database.windows.net,1433;Database=myDataBase;User ID=mylogin@myserver;Password=myPassword;Trusted_Connection=False;Encrypt=True;MultipleActiveResultSets=True;"
+            (Get-PSDBConnectionString)["MARS Enabled"] | Should -BeLikeExactly $cs
+        }
+    }
+}
