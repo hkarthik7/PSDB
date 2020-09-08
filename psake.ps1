8494f2e8f82d60e7ede5d29b2966d7ecaed6b390
Harish <harish.karthic@gmail.com>
2020-09-08 14:12:15 +0100

    Added required modules

--- psake.ps1
+++ psake.ps1
@@ -1,7 +1,8 @@
 # Install dependencies
-$RequiredModules = @("psake", "Pester", "BuildHelpers", "PSScriptAnalyzer")
+$RequiredModules = @("psake", "Pester", "BuildHelpers", "PSScriptAnalyzer", "platyPS")
 $RequiredModules | ForEach-Object {
     if (-not (Get-Module -ListAvailable $_)) {
+        Write-Output "Installing module $($_)"
         Install-Module -Name $_ -SkipPublisherCheck -Scope CurrentUser -Force -Repository PSGallery
     }
 }
