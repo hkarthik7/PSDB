33984702bccd962e99b54aadbc7665833284077b
Harish <harish.karthic@gmail.com>
2020-09-08 06:35:58 +0100

    Added logic for installing dependencies

--- psake.ps1
+++ psake.ps1
@@ -1,1 +1,9 @@
+# Install dependencies
+$RequiredModules = @("psake", "Pester", "BuildHelpers", "PSScriptAnalyzer")
+$RequiredModules | ForEach-Object {
+    if (-not (Get-Module -ListAvailable $_)) {
+        Install-Module -Name $_ -SkipPublisherCheck -Scope CurrentUser -Force -Repository PSGallery
+    }
+}
+
 Invoke-psake .\build.ps1 Clean, Build, Analyze, Test
