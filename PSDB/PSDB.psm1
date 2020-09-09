e6b42bc524b197020094328085522ae2de037ed0
Harish <harish.karthic@gmail.com>
2020-09-09 16:32:50 +0100

    Added keyvault supress message and updated module

--- PSDB/PSDB.psm1
+++ PSDB/PSDB.psm1
@@ -2,8 +2,14 @@
 . "$PSScriptRoot\PSDB.classes.ps1"
 . "$PSScriptRoot\PSDB.functions.ps1"
 
-#Supress the warning message for Key vault
-Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
+# Supress the warning message for KeyVault
+if (-not ($env:SuppressAzurePowerShellBreakingChangeWarnings)) {
+    $env:SuppressAzurePowerShellBreakingChangeWarnings = $true
+}
 
-# Adding alias for older version
-Set-Alias -Name PSDBDefaults -Value Set-PSDBDefault
+# Set default subscriptions in the session
+if ([string]::IsNullOrWhiteSpace($env:PSDB_SUBSCRIPTIONS)) {
+    $Subscriptions = (Get-AzContext -ListAvailable -WarningAction SilentlyContinue).Subscription.Name -join ","
+    [System.Environment]::SetEnvironmentVariable("PSDB_SUBSCRIPTIONS", $Subscriptions, "Process")
+    [PSDBResources]::Subscriptions = $env:PSDB_SUBSCRIPTIONS
+}
